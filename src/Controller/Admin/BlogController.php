f7ff5aab7f8c476036b0d3bc3b9c3edecdd67390
Nofiniaina <nofyrnd@gmail.com>
2026-01-27 13:09:20 +0300

    add default layout

---
+++ src/Controller/Admin/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    #[Route('/admin/blog', name: 'admin.blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class BlogController extends AbstractController
 {
-    #[Route('/welcome', name: 'welcome')]
+    #[Route('/blog', name: 'blog')]
     public function index(): Response
     {
-        return $this->render('welcome.html.twig', [
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
         ]);
     }
 }
