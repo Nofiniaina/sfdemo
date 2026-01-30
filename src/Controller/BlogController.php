2cf6ba389c9c113b6c1ac8f5d4377c18caaa659b
Nofiniaina <nofyrnd@gmail.com>
2026-01-30 09:47:27 +0300

    create design for post create form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -20,6 +20,13 @@ final class BlogController extends AbstractController
         ]);
     }
 
+    #[Route('/blog/post/create', name:'blog.post.create')]
+    public function create() {
+        return $this->render('blog/create.html.twig', [
+            
+        ]);
+    }
+
     #[Route('/blog/post/{slug}', name: 'blog.post.view')]
     public function view(String $slug, PostRepository $postRepo) {
         $post = $postRepo->findOneBy(['slug' => $slug]);
